1f725d8a1ea0c468e33409b26c88dd7a423ce01a
Nomso Ikem <ikemnomso@gmail.com>
2024-03-29 11:04:33 +0000

    update template with user information

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -10,7 +10,7 @@ import React from "react";
  * About background image
  *
  * Below is a sample image. Upload the image of your choice into the "images"
- * directory and import here for use. Then, set imageAltText to string that 
+ * directory and import here for use. Then, set imageAltText to string that
  * represents what you see in that image.
  *
  * Need an image? Check out https://unsplash.com to download a image you
@@ -24,19 +24,17 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "With a background in computer engineering, software development was a natural segue for me. I have collaborated with other developers to work on projects involving design and development using Version Control Systems";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
+  "Frontend development",
   "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Responsive web design",
+  "Backend development",
 ];
 
 /**
@@ -45,7 +43,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I build useful and functional web applications for innovative startups and companies.";
 
 const About = () => {
   return (
--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,29 +31,35 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
-    description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+    title: "IP Address Tracker",
+    description: "A simple IP address tracker using React and the IP Geolocation API.",
+    url: "https://ipaddresstrackerr.netlify.app/",
   },
   {
-    title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+    title: "Portfolio Site",
+    description: "A client portfolio site built with NextJS and Tailwind",
+    url: "https://georgeikem.com/",
   },
   {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+    title: "Space Tourism Site",
+    description: "A responsive website built with resources from frontendmentor.com",
+    url: "https://swish933.github.io/space-tourism-site/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+    title: "Blogr Landing Page",
+    description: "A responsive landing page",
+    url: "https://swish933.github.io/blogr-landing-page/",
   },
+  {
+    title: "Starbucks Page",
+    description: "Unofficial starbucks rewards page",
+    url: "https://swish933.github.io/starbucks-page-clone/",
+  },
+  {
+    title: "Manage Page",
+    description: "A responsive page clone",
+    url: "https://swish933.github.io/tailwind-manage-landing/",
+  }
 ];
 
 const Portfolio = () => {
